849fbfc1d187e03c27f70bb726372d2ff468ba1d
Mohamed <138633077+MohamedMaghrebi@users.noreply.github.com>
2024-02-23 12:24:35 -0330

    Add files via upload

---
+++ Database_Component/Adding things to customer table.sql
@@ -0,0 +1,5 @@
+insert into customer (customer_ID, name, address, phone, email) values ('99999', 'Dianemarie Kilpatrick', '26 Thackeray Street', '631-597-1024', 'dkilpatrick0@webnode.com');
+insert into customer (customer_ID, name, address, phone, email) values ('12344', 'Gino Steuhlmeyer', '29 Riverside Lane', '686-890-2688', 'gsteuhlmeyer1@bandcamp.com');
+insert into customer (customer_ID, name, address, phone, email) values ('56678', 'Rani Filyukov', '96448 Farmco Trail', '811-706-0476', 'rfilyukov2@about.com');
+insert into customer (customer_ID, name, address, phone, email) values ('23441', 'Scotti Whiles', '86 Memorial Alley', '979-853-3895', 'swhiles3@answers.com');
+insert into customer (customer_ID, name, address, phone, email) values ('23444', 'Fransisco Millier', '45 Melby Terrace', '543-727-0128', 'fmillier4@amazon.co.uk');
